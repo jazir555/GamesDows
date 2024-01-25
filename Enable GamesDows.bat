0b31acb9bcb6d379b1a17732a3f8d74995f29b53
jazir5 <31750063+jazir555@users.noreply.github.com>
2024-01-25 15:47:55 -0800

    Update Enable GamesDows.bat

--- Enable GamesDows.bat
+++ Enable GamesDows.bat
@@ -130,9 +130,6 @@ reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\FileSystem" /v Ntfs
 :: Optimize Paging File Performance
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v DisablePagingExecutive /t REG_DWORD /d 1 /f
 
-:: Speed Up Shutdown Process
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control" /v WaitToKillServiceTimeout /t REG_SZ /d 2000 /f
-
 :: Disable Startup Delay
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Serialize" /f
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Serialize" /v StartupDelayInMSec /t REG_DWORD /d 0 /f
