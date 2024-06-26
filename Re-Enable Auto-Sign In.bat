600e1cce1deacec9fc7d3239bb638e6fbcdcafbe
jazir5 <31750063+jazir555@users.noreply.github.com>
2024-06-25 17:05:04 -0700

    Update Re-Enable Auto-Sign In.bat

--- Re-Enable Auto-Sign In.bat
+++ Re-Enable Auto-Sign In.bat
@@ -1,9 +1,10 @@
 @echo off
 SETLOCAL EnableExtensions EnableDelayedExpansion
 
-:: Set your username and password
+:: Set your username, password, and computer name
 SET "USERNAME=your_username"
 SET "PASSWORD=your_password"
+SET "COMPUTER_NAME=%COMPUTERNAME%"
 
 :: Enable AutoAdminLogon
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 1 /f
@@ -14,5 +15,30 @@ REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultU
 :: Set DefaultPassword
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d "%PASSWORD%" /f
 
+:: Set DefaultDomainName to the computer name
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultDomainName /t REG_SZ /d "%COMPUTER_NAME%" /f
+
+:: Create VBScript to handle netplwiz automation
+echo Set WshShell = CreateObject("WScript.Shell") > ConfigureAutoLogin.vbs
+echo WshShell.Run "netplwiz", 1, True >> ConfigureAutoLogin.vbs
+echo WScript.Sleep 500 >> ConfigureAutoLogin.vbs
+echo WshShell.SendKeys "{TAB}" >> ConfigureAutoLogin.vbs
+echo WshShell.SendKeys "{TAB}" >> ConfigureAutoLogin.vbs
+echo WshShell.SendKeys "{ENTER}" >> ConfigureAutoLogin.vbs
+echo WScript.Sleep 500 >> ConfigureAutoLogin.vbs
+echo WshShell.SendKeys "%USERNAME%" >> ConfigureAutoLogin.vbs
+echo WshShell.SendKeys "{TAB}" >> ConfigureAutoLogin.vbs
+echo WshShell.SendKeys "%PASSWORD%" >> ConfigureAutoLogin.vbs
+echo WshShell.SendKeys "{TAB}" >> ConfigureAutoLogin.vbs
+echo WshShell.SendKeys "%PASSWORD%" >> ConfigureAutoLogin.vbs
+echo WshShell.SendKeys "{ENTER}" >> ConfigureAutoLogin.vbs
+echo WScript.Sleep 500 >> ConfigureAutoLogin.vbs
+echo WshShell.SendKeys "{ENTER}" >> ConfigureAutoLogin.vbs
+
+:: Run the VBScript
+cscript //nologo ConfigureAutoLogin.vbs
+
+:: Clean up
+del ConfigureAutoLogin.vbs
+
 echo Automatic login enabled with the specified username and password.
-pause
