531da7ee39d66a98e4568d9e87420ee77f3869c0
jazir5 <31750063+jazir555@users.noreply.github.com>
2024-06-28 15:08:02 -0700

    Fixed VBS Script creation location

--- Enable GamesDows.bat
+++ Enable GamesDows.bat
@@ -43,13 +43,13 @@ echo Creating RunBatchSilently.vbs script
 :: Create VBScript to run the batch file silently
 @echo off
 SET "VBS_NAME=RunBatchSilently.vbs"
-SET "VBS_PATH=%STEAM_FOLDER%\%VBS_NAME%"
+SET "VBS_PATH=%SCRIPT_PATH%\%VBS_NAME%"
 echo Set WshShell = CreateObject("WScript.Shell") > "%VBS_PATH%"
 echo WshShell.Run chr(34)^&"%SCRIPT_PATH%"^&chr(34), 0, True >> "%VBS_PATH%"
 echo Set WshShell = Nothing >> "%VBS_PATH%"
 
 echo Create XML file for the scheduled task
-SET XML_PATH=%STEAM_FOLDER%\DelayedExplorerStartTask.xml
+SET XML_PATH=%SCRIPT_PATH%\DelayedExplorerStartTask.xml
 
 echo Delete the existing XML file if it exists
 IF EXIST "%XML_PATH%" DEL "%XML_PATH%"
