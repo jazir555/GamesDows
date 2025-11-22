8c8d65ae35decb2c9326a85e5b472c25098617bd
jazir5 <31750063+jazir555@users.noreply.github.com>
2025-11-21 20:41:16 -0800

    Update Enable GamesDows Steam V3.bat

--- Enable GamesDows Steam V3.bat
+++ Enable GamesDows Steam V3.bat
@@ -25,10 +25,10 @@ SETLOCAL EnableExtensions EnableDelayedExpansion
 echo Running with administrative privileges...
 echo Setting Steam Big Picture as default shell
 
-
 echo Set Steam Big Picture as the default shell
 SET "KEY_NAME=HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 SET "VALUE_NAME=Shell"
+:: Added -silent to Steam args to prevent popups if possible, though BigPicture overrides it often
 SET "STEAM_PATH=C:\Program Files (x86)\Steam\Steam.exe -bigpicture -nobootstrapupdate -skipinitialbootstrap -skipverifyfiles"
 REG ADD "%KEY_NAME%" /v %VALUE_NAME% /t REG_SZ /d "%STEAM_PATH%" /f
 
@@ -41,26 +41,28 @@ SET "STEAM_EXE=C:\Program Files (x86)\Steam\Steam.exe"
 SET "MANIFEST_PATH=%STEAM_EXE%.manifest"
 
 echo Copying pre-created manifest file...
-copy "%~dp0steam.manifest" "%MANIFEST_PATH%" >nul 2>&1
+:: Ensure steam.manifest exists in the same folder as this script, otherwise skip
+if exist "%~dp0steam.manifest" copy "%~dp0steam.manifest" "%MANIFEST_PATH%" >nul 2>&1
 
 echo Creating DelayedExplorerStart.bat script
-
 echo Create the DelayedExplorerStart.bat script in the Steam folder
 (
 echo @echo off
-echo Check if user is logged on
-echo whoami ^| find /i "%USERNAME%" ^>nul
-echo if ERRORLEVEL 1 exit
-echo Set Shell back to Explorer
+echo :: Ensure Explorer will load as full shell
 echo REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d "%EXPLORER_PATH%" /f
+
+echo :: Wait for Steam Big Picture to initialize
 echo timeout /t 20 /nobreak ^>nul
-echo powershell -WindowStyle Hidden -NoProfile -Command "Start-Process explorer.exe"
+
+echo :: Launch the full Windows Explorer shell
+echo start "" "%EXPLORER_PATH%"
+
+echo :: Allow Explorer to settle
 echo timeout /t 10 /nobreak ^>nul
+
+echo :: Restore Steam as shell for next boot
 echo REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d "%STEAM_PATH%" /f
 ) > "%SCRIPT_PATH%"
-
-
-echo %VBS_PATH% 
 
 echo Creating RunBatchSilently.vbs script
 
@@ -68,77 +70,38 @@ echo Creating RunBatchSilently.vbs script
 @echo off
 SET "VBS_NAME=RunBatchSilently.vbs"
 SET "VBS_PATH=%STEAM_FOLDER%\%VBS_NAME%"
-echo Set WshShell = CreateObject("WScript.Shell") > "%VBS_PATH%"
-echo WshShell.Run chr(34)^&"%SCRIPT_PATH%"^&chr(34), 0, True >> "%VBS_PATH%"
-echo Set WshShell = Nothing >> "%VBS_PATH%"
-
-echo Create XML file for the scheduled task
-SET "XML_PATH=%STEAM_FOLDER%\DelayedExplorerStartTask.xml"
-
-echo Delete the existing XML file if it exists
-IF EXIST "%XML_PATH%" DEL "%XML_PATH%"
-
 (
-echo(<?xml version="1.0" encoding="UTF-16"?>
-echo(^<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task"^>
-echo(  ^<RegistrationInfo^>
-echo(    ^<Date^>2020-01-01T00:00:00^</Date^>
-echo(    ^<Author^>!USERNAME!^</Author^>
-echo(    ^<Description^>Run DelayedExplorerStart.bat at logon.^</Description^>
-echo(  ^</RegistrationInfo^>
-echo(  ^<Triggers^>
-echo(    ^<LogonTrigger^>
-echo(      ^<Enabled^>true^</Enabled^>
-echo(    ^</LogonTrigger^>
-echo(  ^</Triggers^>
-echo(  ^<Principals^>
-echo(    ^<Principal id="Author"^>
-echo(      ^<UserId^>!USERNAME!^</UserId^>
-echo(      ^<LogonType^>InteractiveToken^</LogonType^>
-echo(      ^<RunLevel^>HighestAvailable^</RunLevel^>
-echo(    ^</Principal^>
-echo(  ^</Principals^>
-echo(  ^<Settings^>
-echo(    ^<MultipleInstancesPolicy^>IgnoreNew^</MultipleInstancesPolicy^>
-echo(    ^<DisallowStartIfOnBatteries^>false^</DisallowStartIfOnBatteries^>
-echo(    ^<StopIfGoingOnBatteries^>false^</StopIfGoingOnBatteries^>
-echo(    ^<AllowHardTerminate^>true^</AllowHardTerminate^>
-echo(    ^<StartWhenAvailable^>true^</StartWhenAvailable^>
-echo(    ^<RunOnlyIfNetworkAvailable^>false^</RunOnlyIfNetworkAvailable^>
-echo(    ^<IdleSettings^>
-echo(      ^<StopOnIdleEnd^>true^</StopOnIdleEnd^>
-echo(      ^<RestartOnIdle^>false^</RestartOnIdle^>
-echo(    ^</IdleSettings^>
-echo(    ^<Enabled^>true^</Enabled^>
-echo(    ^<Hidden^>false^</Hidden^>
-echo(    ^<WakeToRun^>false^</WakeToRun^>
-echo(    ^<ExecutionTimeLimit^>PT72H^</ExecutionTimeLimit^>
-echo(    ^<Priority^>7^</Priority^>
-echo(  ^</Settings^>
-echo(  ^<Actions Context="Author"^>
-echo(    ^<Exec^>
-echo(      ^<Command^>wscript.exe^</Command^>
-echo(      ^<Arguments^>"!VBS_PATH!"^</Arguments^>
-echo(    ^</Exec^>
-echo(  ^</Actions^>
-echo(^</Task^>
-) > "%XML_PATH%"
+echo Set WshShell = CreateObject^("WScript.Shell"^)
+echo WshShell.Run chr^(34^) ^& "%SCRIPT_PATH%" ^& chr^(34^), 0, True
+echo Set WshShell = Nothing
+) > "%VBS_PATH%"
 
-echo Delete the existing scheduled task if it exists
-schtasks /delete /tn "RunDelayedExplorerStart" /f 
+echo ---------------------------------------------------
+echo Creating Scheduled Task (Native Method)
+echo ---------------------------------------------------
 
-echo Create the scheduled task using the XML file
-schtasks /create /tn "RunDelayedExplorerStart" /xml "%XML_PATH%" /ru "%USERNAME%" /rp ""
+:: Delete the existing scheduled task if it exists
+schtasks /delete /tn "RunDelayedExplorerStart" /f >nul 2>&1
 
-echo Delayed Explorer start script and VBScript created in Steam folder.
-echo Scheduled Task added to run the script at logon.
-echo XML file for Scheduled Task created.
+:: Create the task using standard flags instead of XML
+:: /sc onlogon : Runs when user logs in
+:: /rl highest : Runs with highest privileges (needed for reg edits in the bat)
+:: /tr : The command to run (The VBS script)
+schtasks /create /tn "RunDelayedExplorerStart" /tr "wscript.exe \"%VBS_PATH%\"" /sc onlogon /rl highest /f
+
+if %ERRORLEVEL% EQU 0 (
+    echo SUCCESS: Scheduled Task created successfully.
+) else (
+    echo ERROR: Failed to create Scheduled Task.
+)
+
+echo ---------------------------------------------------
+echo Applying Registry Optimizations...
 
 echo Disable the boot UI
 bcdedit.exe -set {globalsettings} bootuxdisabled on
 
 echo Disable Logon UI
-
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DisableLogonUI /t REG_DWORD /d 1 /f
 
 echo Disable Visual Effects
@@ -151,19 +114,23 @@ echo Optimize Paging File Performance
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v DisablePagingExecutive /t REG_DWORD /d 1 /f
 
 echo Disable Startup Delay
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Serialize" /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Serialize" /f >nul 2>&1
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Serialize" /v StartupDelayInMSec /t REG_DWORD /d 0 /f
 
 echo Improve Windows Explorer Process Priority
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\explorer.exe\PerfOptions" /v CpuPriorityClass /t REG_DWORD /d 3 /f
+
 echo Adjust Large System Cache
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v LargeSystemCache /t REG_DWORD /d 1 /f
+
 echo Enabling No GUI Boot
 bcdedit /set {current} quietboot on
 
-echo Registry modifications are complete.
-echo Steam Big Picture set as default shell.
-echo Automatic logon enabled.
-echo Boot UI disabled.
-
+echo.
+echo ===================================================
+echo  INSTALLATION COMPLETE
+echo ===================================================
+echo  Steam Big Picture is now the default shell.
+echo  A background task will launch Explorer 20s after login.
+echo  Please Restart your PC to take effect.
 pause
